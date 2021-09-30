536a0cd1113698fd2a9fab8a73fa449090050d7b
Allen <asantmier3@gatech.edu>
2021-09-30 17:52:53 -0400

    Add random pulsating behavior
    I think it looks cooler desynchronized

--- Aframe/material-modify.js
+++ Aframe/material-modify.js
@@ -5,18 +5,30 @@ AFRAME.registerComponent('modify-materials', {
         innerColor: {default: 'white'}
     },
     init: function () {
-        // Wait for model to load.
+      // Wait for model to load.
       this.el.addEventListener('model-loaded', () => {
-          // Grab the mesh / scene.
+        this.startTime = new Date();
+        this.wait = Math.random() * (10000 - 1) + 1;
+        // Grab the mesh / scene.
         const obj = this.el.getObject3D('mesh');
         // Go over the submeshes and modify materials we want.
         obj.traverse(node => {
-          if (node.name.indexOf('Frame') !== -1) {
-            node.material.color.set(this.data.frameColor);
-          } else if (node.name.indexOf('InnerX') !== -1 || node.name.indexOf('InnerY') !== -1 || node.name.indexOf('InnerZ') !== -1) {
-            node.material.color.set(this.data.innerColor);
-          }
-        });
+        if (node.name.indexOf('Frame') !== -1) {
+          node.material.color.set(this.data.frameColor);
+        } else if (node.name.indexOf('InnerX') !== -1 || node.name.indexOf('InnerY') !== -1 || node.name.indexOf('InnerZ') !== -1) {
+          node.material.color.set(this.data.innerColor);
+        }
       });
+    });
+  },
+  tick: function () {
+    if (this.startTime) {
+      let endTime = new Date();
+      let timeDiff = endTime - this.startTime;
+      if (timeDiff >= this.wait) {
+        this.el.setAttribute("animation-mixer", "clip", "*")
+        startTime = null
+      }
     }
-  });
+  }
+});
